593562c29b4326e0fe68033b8f8fe7b2891656f0
Billy Soup <joeythesoda@gmail.com>
2020-10-15 20:29:39 -0400

    added crop image

--- handlers/Image/ImageManipulator.java
+++ handlers/Image/ImageManipulator.java
@@ -1,5 +1,11 @@
 package handlers.Image;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
+
 import handlers.Handler;
 import util.Message;
 
@@ -8,7 +14,23 @@ public class ImageManipulator implements Handler {
     @Override
     public void handleMessage(Message m) {
         // TODO Auto-generated method stub
+        if (m.getText().startsWith("!imgcrop")) {
+            Scanner scanner = new Scanner(m.getText());
+            scanner.next();
+            String name = scanner.next();
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            int width = scanner.nextInt();
+            int height = scanner.nextInt();
+            cropImage(name, x, y, width, height);
+            m.reply(new Message(null, "image", "cropped"));
+        }
+    }
 
+    public void cropImage(String name, int x, int y, int width, int height) {
+        BufferedImage src = (BufferedImage)ImageIO.read(new File(name));
+        BufferedImage dest = src.getSubimage(x, y, width, height);
+        ImageIO.write(dest, "png", new File(name));
     }
     
 }
