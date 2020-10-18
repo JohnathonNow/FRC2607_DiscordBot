561c64ef4bc3ed89df0ac73564ef995ef46bb0c2
CrunchyCuber <3jwproduction@gmail.com>
2020-10-17 20:14:38 -0400

    added simulateOne code. Needs to be reviewed

--- handlers/FRC/MatchPredictor.java
+++ handlers/FRC/MatchPredictor.java
@@ -12,10 +12,29 @@ public class MatchPredictor implements Handler {
     @Override
     public void handleMessage(Message m) {
         throw new UnsupportedOperationException("NOT IMPLEMENTED");
+        
     }
 
     public Result simulateOne(Match m) {
-        throw new UnsupportedOperationException("NOT IMPLEMENTED");
+        int i = 0; 
+        int blueScore = 0;
+        int redScore = 0;
+        while(i<3){
+            if(data.isPlayingMatch(m.getBlueAlliance()[i]) && !data.isPlayingDefense(m.getBlueAlliance()[i])){
+                blueScore = blueScore + data.getPossibleScore(m.getBlueAlliance()[i]);
+            }
+            if(data.isPlayingMatch(m.getRedAlliance()[i]) && !data.isPlayingDefense(m.getRedAlliance()[i])){
+                redScore = redScore + data.getPossibleScore(m.getRedAlliance()[i]);
+            }
+            i++;
+        }
+        if(redScore<blueScore){
+            return Result.BLUE_WIN;
+        } else if (redScore>blueScore){
+            return Result.RED_WIN;
+        } else {
+            return Result.TIE;
+        }
     }
 
     public Prediction simulateMany(Match m) {
